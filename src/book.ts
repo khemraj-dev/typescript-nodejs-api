6955a3b9c23453311ebdf234cf9d2325dc97b376
khemraj-dev <khemraj.47billion@gmail.com>
2019-01-13 12:56:04 +0530

    add nested schema payload

--- src/book.ts
+++ src/book.ts
@@ -21,7 +21,74 @@ export interface IBook extends mongoose.Document {
 export const BookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
+    examSubjects: [
+        {
+            class: [
+                {
+                    section: [
+                        {
+                            subject: String
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
 });
 
 const Book = mongoose.model('Book', BookSchema, 'Book');
 export default Book;
+
+
+// payload
+// {
+// 	"title": "BookTitle",
+// 	"author": "BookAuther",
+// 	"examSubjects": [{
+// 			"class": [{
+// 				"section": [{
+// 						"subject": "hindi2"
+// 					}, {
+// 						"subject": "english"
+// 					},
+// 					{
+// 						"subject": "science"
+// 					}
+// 				]
+// 			}, {
+// 				"section": [{
+// 						"subject": "match"
+// 					}, {
+// 						"subject": "history"
+// 					},
+// 					{
+// 						"subject": "computer"
+// 					}
+// 				]
+// 			}]
+// 		},
+// 		{
+// 			"class": [{
+// 				"section": [{
+// 						"subject": "hindi2"
+// 					}, {
+// 						"subject": "english"
+// 					},
+// 					{
+// 						"subject": "science"
+// 					}
+// 				]
+// 			}, {
+// 				"section": [{
+// 						"subject": "match"
+// 					}, {
+// 						"subject": "history"
+// 					},
+// 					{
+// 						"subject": "computer"
+// 					}
+// 				]
+// 			}]
+// 		}
+// 	]
+// }
